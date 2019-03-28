9e64ee6feedc71f2cfa6c6a66948900782741e07
vinayk96vk <46961865+vinayk96vk@users.noreply.github.com>
2019-03-28 15:28:34 +0530

    Add files via upload

---
+++ Loginpg.sql
@@ -0,0 +1,16 @@
+﻿create database Mydb
+use Mydb
+
+create table tblUser
+(id varchar(20) primary key,
+user_password varchar(20),
+user_role varchar(20));
+insert into tblUser values('Jeff','1234','user')
+go
+create proc prologic(@id varchar(20),@pass varchar(20))
+as
+begin
+select user_role from tblUser where id=@id and user_password=@pass
+end
+go
+execute prologic 'Jeff','1234'
